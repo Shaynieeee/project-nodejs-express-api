d9eb2eb6344123c02c568e9b942fad7a2c4c9658
Frandy Jaya <frandy.jaya@gojek.com>
2024-07-28 20:45:02 +0700

    disable database mongo

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const config = require('./config');
 
 const app = express();
 const router = express.Router();
 
-mongoose.connect(config.CONNECTION_STRING);
+// mongoose.connect(config.CONNECTION_STRING);
 
 //load models
 const User = require('./models/user');
