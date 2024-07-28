83b62d172bc077c9bfdd3e2793c7ab85d1bf54c1
Frandy Jaya <frandy.jaya@gojek.com>
2024-07-28 20:47:47 +0700

    rename project

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-global.PROJECT_NAME = 'REST API';
+global.PROJECT_NAME = 'Shaynie Catalog API';
 global.PROJECT_URL = 'http://localhost:3000';
 global.PROJECT_VERSION = '0.0.1';
 global.SALT_KEY = 'e8ccfba6-f087-4697-a567-2ee41e6b6697'; //important
